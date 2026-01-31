1f3b76928efcdb3a4a10ee7cfc612c9daa0b1741
contramundum53 <contramundum53@outlook.com>
2026-01-31 11:34:06 +0900

    Disable animations outside Chrome

--- public/app.js
+++ public/app.js
@@ -1,5 +1,13 @@
 import init from "./pkg/yumboard_client.js";
 
+const ua = navigator.userAgent || "";
+const isChrome =
+  (ua.includes("Chrome") && !ua.includes("Edg") && !ua.includes("OPR")) ||
+  ua.includes("CriOS");
+if (isChrome) {
+  document.body.classList.add("is-chrome");
+}
+
 init().catch((err) => {
   console.error("Failed to start WASM client", err);
 });
